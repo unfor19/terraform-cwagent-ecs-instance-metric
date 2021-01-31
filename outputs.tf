b22abf1ed2fb16e75a0c089ced5505b9b63c57a7
Meir Gabay <meir@prodops.io>
2021-02-01 00:59:11 +0200

    added outputs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,24 @@
-output "aws_ecs_service_arn" {
+output "app_name" {
+  value       = local.app_name
+  description = "Final name of the app, optionally includes prefix and suffix"
+}
+
+output "ecs_service_arn" {
   value       = aws_ecs_service.cwagent.arn
   description = "Service ARN"
+}
+
+output "ecs_task_definition_arn" {
+  value       = aws_ecs_task_definition.cwagent.ARN
+  description = "Task definition ARN"
+}
+
+output "iam_cwagent_task_execution_role" {
+  value       = aws_iam_role.cwagent_task_execution_role.arn
+  description = "Task execution role ARN"
+}
+
+output "cwagent_task_role" {
+  value       = aws_iam_role.cwagent_task_role.arn
+  description = "Task role ARN"
 }
